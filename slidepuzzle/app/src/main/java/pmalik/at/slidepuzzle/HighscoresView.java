fde15226805f377dbc182045c7eb31b4df4b0349
PatrickMalik <patrick.malik@hotmail.com>
2017-03-31 13:36:32 +0200

    First few Views, not really anything happened yet

---
+++ slidepuzzle/app/src/main/java/pmalik/at/slidepuzzle/HighscoresView.java
@@ -0,0 +1,18 @@
+package pmalik.at.slidepuzzle;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Austr on 31.03.2017.
+ */
+
+public class HighscoresView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highscores_view);
+    }
+}
---
+++ slidepuzzle/app/src/main/java/pmalik/at/slidepuzzle/MainMenu.java
@@ -0,0 +1,42 @@
+package pmalik.at.slidepuzzle;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainMenu extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+    }
+
+    public void switchToStart(View view){
+        Intent intent = new Intent(MainMenu.this, GameView.class);
+        startActivity(intent);
+    }
+
+    public void switchToOptions(View view){
+        Intent intent = new Intent(MainMenu.this, OptionsView.class);
+        startActivity(intent);
+    }
+
+    public void switchToHighscores(View view){
+        Intent intent = new Intent(MainMenu.this, HighscoresView.class);
+        startActivity(intent);
+    }
+
+
+}
