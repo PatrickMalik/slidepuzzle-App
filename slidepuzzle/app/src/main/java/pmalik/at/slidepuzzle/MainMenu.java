5d64f901a7af60707e2d6de69fdc7edf4dee0fba
PatrickMalik <patrick.malik@hotmail.com>
2017-04-02 17:57:33 +0200

    Added the highscore screen and tried to let the title bar vanish but didnt work yet

--- slidepuzzle/app/src/main/java/pmalik/at/slidepuzzle/MainMenu.java
+++ slidepuzzle/app/src/main/java/pmalik/at/slidepuzzle/MainMenu.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -19,6 +20,7 @@ public class MainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main_menu);
 
     }
